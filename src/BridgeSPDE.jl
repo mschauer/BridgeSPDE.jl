0d82d9f6eca3b1dc35f3f10ca01bc764a8031881
mschauer <moritzschauer@web.de>
2020-01-17 15:28:37 +0100

    Add module

--- src/BridgeSPDE.jl
+++ src/BridgeSPDE.jl
@@ -1,5 +1,17 @@
 module BridgeSPDE
+using Bridge
+import Bridge: outer, b, a
 
-greet() = print("Hello World!")
+using GaussianDistributions, Trajectories, DynamicIterators
+using LinearAlgebra, FileIO, Random, Statistics, SparseArrays, Colors
 
+using DynamicIterators: dub
+import DynamicIterators: evolve, dyniterate
+
+export gridderiv, gridlaplacian, OrnsteinUhlenbeck, b, riccatti
+export downsample, boundary
+export Euler
+
+include("laplace.jl")
+include("ornsteinuhlenbeck.jl")
 end # module
